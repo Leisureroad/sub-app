d0c9eddff0dadf7c757cceccfa4dca573ac0992a
Fan Liu <charles_liuf@hotmail.com>
2018-08-20 22:02:17 +0800

    access pub-svc

--- src/main/java/com/example/SubSvc/SubSvcApplication.java
+++ src/main/java/com/example/SubSvc/SubSvcApplication.java
@@ -1,9 +1,16 @@
 package com.example.SubSvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @SpringBootApplication
 @RestController
@@ -12,6 +19,22 @@ public class SubSvcApplication {
 	@RequestMapping("/")
 	public String hello() {
 	  return "Hello World!";
+	}
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@RequestMapping("/test-pub-svc")
+	public String foo() {
+		URI uri = UriComponentsBuilder.fromUriString("//pub-svc:8080/").build().toUri();
+		String result = restTemplate.getForObject(uri,String.class);
+		return result;
 	}
 
 	public static void main(String[] args) {
